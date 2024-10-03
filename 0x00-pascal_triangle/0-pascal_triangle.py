df863a81bc06030f6d819c8ea3a88bcf8e79b779
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-10-03 22:56:26 +0100

    updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,14 +3,14 @@ def pascal_triangle(n):
     """A script to determine pascal's triangle for any number"""
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
-    for i in range(1,n):
+
+    for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
-        
+
         triangle.append(row)
     return triangle
